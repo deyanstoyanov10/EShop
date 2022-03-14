27a8a3734dcb27627a24836943ce07365a1e77cf
Deyan Stoyanov <didko985@gmail.com>
2022-03-14 21:23:55 +0200

    Add ApiService abstraction for the client

--- EShop/EShop.Client/Services/ApiService.cs
+++ EShop/EShop.Client/Services/ApiService.cs
@@ -1,7 +1,101 @@
 ﻿namespace EShop.Client.Services
 {
-    internal class ApiService
-    {
+    using Common;
+    using Common.ApiResponse;
 
+    using Blazored.LocalStorage;
+
+    using System.Net.Http.Json;
+    using System.Net.Http.Headers;
+
+    internal abstract class ApiService
+    {
+        protected readonly string JWT_STORAGE_KEY = "jwtToken_v1";
+        protected readonly HttpClient _httpClient;
+        protected readonly ILocalStorageService _localStorage;
+
+        private readonly string INTERNAL_ERROR_MESSAGE = "Internal Error";
+
+        protected ApiService(
+            HttpClient httpClient,
+            ILocalStorageService localStorage)
+        {
+            _httpClient = httpClient;
+            _localStorage = localStorage;
+        }
+
+        protected async Task<ApiResponse<T>> GetJson<T>(string url)
+        {
+            var jwtToken = await this.GetJwtToken();
+
+            if (isValidToken(jwtToken))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
+            }
+
+            try
+            {
+                var jsonObject = await _httpClient.GetFromJsonAsync<ApiResponse<T>>(url);
+                return jsonObject ?? new ApiResponse<T>(new List<Error> { new Error(INTERNAL_ERROR_MESSAGE) });
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse<T>(new List<Error> { new Error(ex.Message) });
+            }
+        }
+
+        protected async Task<ApiResponse<TResponse>> PostJson<TRequest, TResponse>(string url, TRequest request)
+        {
+            var jwtToken = await this.GetJwtToken();
+
+            if (isValidToken(jwtToken))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
+            }
+
+            try
+            {
+                var response = await _httpClient.PostAsJsonAsync(url, request);
+                var jsonObject = await response.Content.ReadFromJsonAsync<ApiResponse<TResponse>>();
+                return jsonObject ?? new ApiResponse<TResponse>(new List<Error> { new Error(INTERNAL_ERROR_MESSAGE) });
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse<TResponse>(new List<Error> { new Error(ex.Message) });
+            }
+        }
+
+        protected async Task<ApiResponse<TResponse>> PutJson<TRequest, TResponse>(string url, TRequest request)
+        {
+            var jwtToken = await this.GetJwtToken();
+
+            if (isValidToken(jwtToken))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", jwtToken);
+            }
+
+            try
+            {
+                var response = await _httpClient.PutAsJsonAsync(url, request);
+                var jsonObject = await response.Content.ReadFromJsonAsync<ApiResponse<TResponse>>();
+                return jsonObject ?? new ApiResponse<TResponse>(new List<Error> { new Error(INTERNAL_ERROR_MESSAGE) });
+            }
+            catch (Exception ex)
+            {
+                return new ApiResponse<TResponse>(new List<Error> { new Error(ex.Message) });
+            }
+        }
+
+        protected async Task<string> GetJwtToken()
+            => await _localStorage.GetItemAsync<string>(JWT_STORAGE_KEY);
+
+        protected async Task SetJwtToken(string jwtToken)
+            => await _localStorage.SetItemAsync(JWT_STORAGE_KEY, jwtToken);
+
+        protected async Task RemoveJwtToken()
+            => await _localStorage.RemoveItemAsync(JWT_STORAGE_KEY);
+
+        private bool isValidToken(string jwtToken)
+            => !string.IsNullOrEmpty(jwtToken) || !string.IsNullOrWhiteSpace(jwtToken);
     }
 }
--- EShop/EShop.Client/Services/Authentication/AuthService.cs
+++ EShop/EShop.Client/Services/Authentication/AuthService.cs
@@ -1,32 +1,21 @@
 ﻿namespace EShop.Client.Services.Authentication
 {
-    using Common;
     using Contracts;
     using Common.ApiResponse;
 
     using Blazored.LocalStorage;
     using Microsoft.AspNetCore.Components.Authorization;
 
-    using System.Net.Http.Json;
-    using System.Net.Http.Headers;
-
     using static EShop.Common.Authentication.AuthenticationRecords;
 
-    internal class AuthService : IAuthService
+    internal class AuthService : ApiService, IAuthService
     {
-        private readonly HttpClient _httpClient;
-        private readonly ILocalStorageService _localStorage;
         private readonly AuthenticationStateProvider _authenticationStateProvider;
 
         public AuthService(
             HttpClient httpClient,
             ILocalStorageService localStorage,
-            AuthenticationStateProvider authenticationStateProvider)
-        {
-            _httpClient = httpClient;
-            _localStorage = localStorage;
-            _authenticationStateProvider = authenticationStateProvider;
-        }
+            AuthenticationStateProvider authenticationStateProvider) : base(httpClient, localStorage) => _authenticationStateProvider = authenticationStateProvider;
 
         public async Task<ApiResponse<AppUserOutputModel>> Login(LoginUserInputModel loginModel)
         {
@@ -37,7 +26,7 @@
                 return result;
             }
 
-            await _localStorage.SetItemAsync("jwtToken_v1", result.Data.token);
+            await this.SetJwtToken(result.Data.token);
             ((AuthStateProvider)_authenticationStateProvider).MarkUserAsAuthenticated(loginModel.Username);
 
             return result;
@@ -45,30 +34,8 @@
 
         public async Task Logout()
         {
-            await _localStorage.RemoveItemAsync("jwtToken_v1");
+            await this.RemoveJwtToken();
             ((AuthStateProvider)_authenticationStateProvider).MarkUserAsLoggedOut();
-            _httpClient.DefaultRequestHeaders.Authorization = null;
-        }
-
-        private async Task<ApiResponse<TResponse>> PostJson<TRequest, TResponse>(string url, TRequest request)
-        {
-            var token = await _localStorage.GetItemAsync<string>("jwtToken_v1");
-
-            if (!string.IsNullOrEmpty(token) || !string.IsNullOrWhiteSpace(token))
-            {
-                _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            }
-
-            try
-            {
-                var response = await _httpClient.PostAsJsonAsync(url, request);
-                var jsonObject = await response.Content.ReadFromJsonAsync<ApiResponse<TResponse>>();
-                return jsonObject;
-            }
-            catch (Exception ex)
-            {
-                return new ApiResponse<TResponse>(new List<Error> { new Error(ex.Message) });
-            }
         }
     }
 }

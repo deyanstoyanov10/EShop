c0e8140d6574f7ea09cffa7473c51932e966ca69
Deyan Stoyanov <didko985@gmail.com>
2022-03-17 21:08:05 +0200

    Fix

---
+++ EShop/EShop.Tests/Routing/AuthControllerRouteTests.cs
@@ -0,0 +1,21 @@
+﻿namespace EShop.Tests.Routing
+{
+    using EShop.API.Controllers;
+    using MyTested.AspNetCore.Mvc;
+    using MyTested.WebApi;
+    using Xunit;
+    using static Common.Authentication.AuthenticationRecords;
+
+    public class AuthControllerRouteTests
+    {
+        private readonly string LoginModelJson = "{\"username\": \"validUsername\",\"password\": \"validPassword\"}";
+
+        //[Fact]
+        //public void PostLoginShouldBeMappedCorrectly()
+        //    => MyWebApi
+        //            .Routes()
+        //            .ShouldMap("api/Books/Get")
+        //            .WithHttpMethod(HttpMethod.Get)
+        //            .To<AuthController>(c => c.Get());
+    }
+}
